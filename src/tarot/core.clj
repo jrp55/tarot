28d4ee7e2dd79c9f59eca697b33d063c462a80ea
James Poole <gipperp@googlemail.com>
2015-05-24 16:04:57 +0000

    Made whitespace changes to the code and added documentation strings

--- src/tarot/core.clj
+++ src/tarot/core.clj
@@ -82,41 +82,76 @@
 		{:card :twentyone :suit :trumps :value 4.5}
 		{:card :excuse :suit :excuse :value 4.5}])
 
-(defn shuffle-deck [] (shuffle (seq cards)))
+(defn shuffle-deck 
+  "Returns a shuffled deck from cards"
+  [] 
+  (shuffle (seq cards)))
 
-(defn show-card [x] (hash-map :suit (:suit x) :card (:card x)))
+(defn show-card 
+  "Shows the :card and :suit of x"
+  [x] 
+  (hash-map :suit (:suit x) :card (:card x)))
 
-(defn show-shuffled-deck [] (map show-card (shuffle-deck)))
+(defn show-shuffled-deck 
+  "Shows the :card and :suit for each card in a newly shuffled deck"
+  [] 
+  (map show-card (shuffle-deck)))
 
-(defn dog-deal? [dealt-dog counter]
-	(and (false? dealt-dog) (pos? counter) (zero? (mod counter 2))))
+(defn dog-deal? 
+  "Returns true if a card should be dealt into the dog and false otherwise"
+  [dealt-dog counter]
+  (and 
+    (false? dealt-dog) 
+    (pos? counter) 
+    (zero? (mod counter 2))))
 
 (def deal-packet-size 3)
 
-(defn deal-packet [deck result counter players]
-	(assoc result (nth players counter) (flatten (conj (if (zero? (count ((nth players counter) result))) [] ((nth players counter) result)) (take deal-packet-size deck)))))
+(defn deal-packet 
+  "Deals a packet of cards to the next player"
+  [deck result counter players]
+  (assoc result 
+    (nth players counter) 
+    (flatten 
+      (conj 
+        (if (zero? (count ((nth players counter) result))) 
+          [] 
+          ((nth players counter) result)) 
+        (take deal-packet-size deck)))))
 
 (def dog-deal-size 1)
 
-(defn deal-dog [deck result]
-	(assoc result :dog (flatten (conj (if (zero? (count (:dog result))) [] (:dog result)) (take dog-deal-size deck)))))
+(defn deal-dog 
+  "Deals a card to the dog"
+  [deck result]
+  (assoc result :dog 
+    (flatten 
+      (conj 
+        (if (zero? (count (:dog result))) 
+          [] 
+          (:dog result)) 
+        (take dog-deal-size deck)))))
 
-(defn deal-int [deck players]
-	(loop [remaining-deck deck
-	       result (hash-map)
-               counter 0
-               dealt-dog false]
-		(let [dealing-dog (dog-deal? dealt-dog counter)]
-			(if (zero? (count remaining-deck))
-				result
-				(recur (nthrest remaining-deck (if dealing-dog dog-deal-size deal-packet-size))
-				       (if dealing-dog
-					(deal-dog remaining-deck result)
-					(deal-packet remaining-deck result counter players))
-				       (if dealing-dog
-                                         counter
-                                         (mod (inc counter) (count players)))
-                                       dealing-dog)))))
+(defn deal-int 
+  "Deals the deck out to the players, and includes a dog" 
+  [deck players]
+  (loop [remaining-deck deck
+	 result (hash-map)
+         counter 0
+         dealt-dog false]
+    (let [dealing-dog (dog-deal? dealt-dog counter)]
+      (if (zero? (count remaining-deck))
+        result
+        (recur (nthrest remaining-deck (if dealing-dog dog-deal-size deal-packet-size))
+	       (if dealing-dog
+                 (deal-dog remaining-deck result)
+                 (deal-packet remaining-deck result counter players))
+               (if dealing-dog
+                 counter
+                 (mod (inc counter) (count players)))
+               dealing-dog)))))
 
-(defn deal-for-players [players]
-	(deal-int (show-shuffled-deck) players))
+(defn deal-for-players 
+  "Deals a tarot deck out to the players"
+  [players]
+  (deal-int (show-shuffled-deck) players))
