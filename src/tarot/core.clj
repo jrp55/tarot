c3742cad8aa6b0e413ddeeed5e27adf0fb7c5f4b
James Poole <gipperp@googlemail.com>
2015-05-25 12:29:06 +0000

    Added :cmpval to all cards

--- src/tarot/core.clj
+++ src/tarot/core.clj
@@ -3,83 +3,83 @@
 
 (def cards
 	[
-		{:card :one :suit :clubs :value 0.5}
-		{:card :two :suit :clubs :value 0.5}
-		{:card :three :suit :clubs :value 0.5}
-		{:card :four :suit :clubs :value 0.5}
-		{:card :five :suit :clubs :value 0.5}
-		{:card :six :suit :clubs :value 0.5}
-		{:card :seven :suit :clubs :value 0.5}
-		{:card :eight :suit :clubs :value 0.5}
-		{:card :nine :suit :clubs :value 0.5}
-		{:card :ten :suit :clubs :value 0.5}
-		{:card :jack :suit :clubs :value 1.5}
-		{:card :chevalier :suit :clubs :value 2.5}
-		{:card :dame :suit :clubs :value 3.5}
-		{:card :roi :suit :clubs :value 4.5}
-		{:card :one :suit :hearts :value 0.5}
-		{:card :two :suit :hearts :value 0.5}
-		{:card :three :suit :hearts :value 0.5}
-		{:card :four :suit :hearts :value 0.5}
-		{:card :five :suit :hearts :value 0.5}
-		{:card :six :suit :hearts :value 0.5}
-		{:card :seven :suit :hearts :value 0.5}
-		{:card :eight :suit :hearts :value 0.5}
-		{:card :nine :suit :hearts :value 0.5}
-		{:card :ten :suit :hearts :value 0.5}
-		{:card :valet :suit :hearts :value 1.5}
-		{:card :chevalier :suit :hearts :value 2.5}
-		{:card :dame :suit :hearts :value 3.5}
-		{:card :roi :suit :hearts :value 4.5}
-		{:card :one :suit :diamonds :value 0.5}
-		{:card :two :suit :diamonds :value 0.5}
-		{:card :three :suit :diamonds :value 0.5}
-		{:card :four :suit :diamonds :value 0.5}
-		{:card :five :suit :diamonds :value 0.5}
-		{:card :six :suit :diamonds :value 0.5}
-		{:card :seven :suit :diamonds :value 0.5}
-		{:card :eight :suit :diamonds :value 0.5}
-		{:card :nine :suit :diamonds :value 0.5}
-		{:card :ten :suit :diamonds :value 0.5}
-		{:card :valet :suit :diamonds :value 1.5}
-		{:card :chevalier :suit :diamonds :value 2.5}
-		{:card :dame :suit :diamonds :value 3.5}
-		{:card :roi :suit :diamonds :value 4.5}
-		{:card :one :suit :spades :value 0.5}
-		{:card :two :suit :spades :value 0.5}
-		{:card :three :suit :spades :value 0.5}
-		{:card :four :suit :spades :value 0.5}
-		{:card :five :suit :spades :value 0.5}
-		{:card :six :suit :spades :value 0.5}
-		{:card :seven :suit :spades :value 0.5}
-		{:card :eight :suit :spades :value 0.5}
-		{:card :nine :suit :spades :value 0.5}
-		{:card :ten :suit :spades :value 0.5}
-		{:card :valet :suit :spades :value 1.5}
-		{:card :chevalier :suit :spades :value 2.5}
-		{:card :dame :suit :spades :value 3.5}
-		{:card :roi :suit :spades :value 4.5}
-		{:card :one :suit :trumps :value 4.5}
-		{:card :two :suit :trumps :value 0.5}
-		{:card :three :suit :trumps :value 0.5}
-		{:card :four :suit :trumps :value 0.5}
-		{:card :five :suit :trumps :value 0.5}
-		{:card :six :suit :trumps :value 0.5}
-		{:card :seven :suit :trumps :value 0.5}
-		{:card :eight :suit :trumps :value 0.5}
-		{:card :nine :suit :trumps :value 0.5}
-		{:card :ten :suit :trumps :value 0.5}
-		{:card :eleven :suit :trumps :value 0.5}
-		{:card :twelve :suit :trumps :value 0.5}
-		{:card :thirteen :suit :trumps :value 0.5}
-		{:card :fourteen :suit :trumps :value 0.5}
-		{:card :fifteen :suit :trumps :value 0.5}
-		{:card :sixteen :suit :trumps :value 0.5}
-		{:card :seventeen :suit :trumps :value 0.5}
-		{:card :eighteen :suit :trumps :value 0.5}
-		{:card :nineteen :suit :trumps :value 0.5}
-		{:card :twenty :suit :trumps :value 0.5}
-		{:card :twentyone :suit :trumps :value 4.5}
+		{:card :one :suit :clubs :cmpval 1 :value 0.5}
+		{:card :two :suit :clubs :cmpval 2 :value 0.5}
+		{:card :three :suit :clubs :cmpval 3 :value 0.5}
+		{:card :four :suit :clubs :cmpval 4 :value 0.5}
+		{:card :five :suit :clubs :cmpval 5 :value 0.5}
+		{:card :six :suit :clubs :cmpval 6 :value 0.5}
+		{:card :seven :suit :clubs :cmpval 7 :value 0.5}
+		{:card :eight :suit :clubs :cmpval 8 :value 0.5}
+		{:card :nine :suit :clubs :cmpval 9 :value 0.5}
+		{:card :ten :suit :clubs :cmpval 10 :value 0.5}
+		{:card :jack :suit :clubs :cmpval 11 :value 1.5}
+		{:card :chevalier :suit :clubs :cmpval 12 :value 2.5}
+		{:card :dame :suit :clubs :cmpval 13 :value 3.5}
+		{:card :roi :suit :clubs :cmpval 14 :value 4.5}
+		{:card :one :suit :hearts :cmpval 1 :value 0.5}
+		{:card :two :suit :hearts :cmpval 2 :value 0.5}
+		{:card :three :suit :hearts :cmpval 3 :value 0.5}
+		{:card :four :suit :hearts :cmpval 4 :value 0.5}
+		{:card :five :suit :hearts :cmpval 5 :value 0.5}
+		{:card :six :suit :hearts :cmpval 6 :value 0.5}
+		{:card :seven :suit :hearts :cmpval 7 :value 0.5}
+		{:card :eight :suit :hearts :cmpval 8 :value 0.5}
+		{:card :nine :suit :hearts :cmpval 9 :value 0.5}
+		{:card :ten :suit :hearts :cmpval 10 :value 0.5}
+		{:card :valet :suit :hearts :cmpval 11 :value 1.5}
+		{:card :chevalier :suit :hearts :cmpval 12 :value 2.5}
+		{:card :dame :suit :hearts :cmpval 13 :value 3.5}
+		{:card :roi :suit :hearts :cmpval 14 :value 4.5}
+		{:card :one :suit :diamonds :cmpval 1 :value 0.5}
+		{:card :two :suit :diamonds :cmpval 2 :value 0.5}
+		{:card :three :suit :diamonds :cmpval 3 :value 0.5}
+		{:card :four :suit :diamonds :cmpval 4 :value 0.5}
+		{:card :five :suit :diamonds :cmpval 5 :value 0.5}
+		{:card :six :suit :diamonds :cmpval 6 :value 0.5}
+		{:card :seven :suit :diamonds :cmpval 7 :value 0.5}
+		{:card :eight :suit :diamonds :cmpval 8 :value 0.5}
+		{:card :nine :suit :diamonds :cmpval 9 :value 0.5}
+		{:card :ten :suit :diamonds :cmpval 10 :value 0.5}
+		{:card :valet :suit :diamonds :cmpval 11 :value 1.5}
+		{:card :chevalier :suit :diamonds :cmpval 12 :value 2.5}
+		{:card :dame :suit :diamonds :cmpval 13 :value 3.5}
+		{:card :roi :suit :diamonds :cmpval 14 :value 4.5}
+		{:card :one :suit :spades :cmpval 1 :value 0.5}
+		{:card :two :suit :spades :cmpval 2 :value 0.5}
+		{:card :three :suit :spades :cmpval 3 :value 0.5}
+		{:card :four :suit :spades :cmpval 4 :value 0.5}
+		{:card :five :suit :spades :cmpval 5 :value 0.5}
+		{:card :six :suit :spades :cmpval 6 :value 0.5}
+		{:card :seven :suit :spades :cmpval 7 :value 0.5}
+		{:card :eight :suit :spades :cmpval 8 :value 0.5}
+		{:card :nine :suit :spades :cmpval 9 :value 0.5}
+		{:card :ten :suit :spades :cmpval 10 :value 0.5}
+		{:card :valet :suit :spades :cmpval 11 :value 1.5}
+		{:card :chevalier :suit :spades :cmpval 12 :value 2.5}
+		{:card :dame :suit :spades :cmpval 13 :value 3.5}
+		{:card :roi :suit :spades :cmpval 14 :value 4.5}
+		{:card :one :suit :trumps :cmpval 1 :value 4.5}
+		{:card :two :suit :trumps :cmpval 2 :value 0.5}
+		{:card :three :suit :trumps :cmpval 3 :value 0.5}
+		{:card :four :suit :trumps :cmpval 4 :value 0.5}
+		{:card :five :suit :trumps :cmpval 5 :value 0.5}
+		{:card :six :suit :trumps :cmpval 6 :value 0.5}
+		{:card :seven :suit :trumps :cmpval 7 :value 0.5}
+		{:card :eight :suit :trumps :cmpval 8 :value 0.5}
+		{:card :nine :suit :trumps :cmpval 9 :value 0.5}
+		{:card :ten :suit :trumps :cmpval 10 :value 0.5}
+		{:card :eleven :suit :trumps :cmpval 11 :value 0.5}
+		{:card :twelve :suit :trumps :cmpval 12 :value 0.5}
+		{:card :thirteen :suit :trumps :cmpval 13 :value 0.5}
+		{:card :fourteen :suit :trumps :cmpval 14 :value 0.5}
+		{:card :fifteen :suit :trumps :cmpval 15 :value 0.5}
+		{:card :sixteen :suit :trumps :cmpval 16 :value 0.5}
+		{:card :seventeen :suit :trumps :cmpval 17 :value 0.5}
+		{:card :eighteen :suit :trumps :cmpval 18 :value 0.5}
+		{:card :nineteen :suit :trumps :cmpval 19 :value 0.5}
+		{:card :twenty :suit :trumps :cmpval 20 :value 0.5}
+		{:card :twentyone :suit :trumps :cmpval 21 :value 4.5}
 		{:card :excuse :suit :excuse :value 4.5}])
 
 (defn shuffle-deck 
