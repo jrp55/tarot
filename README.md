31f1b9815f9e3bf4e63214d38c9179e66c0ac7cc
James Poole <gipperp@googlemail.com>
2015-05-29 22:11:02 +0000

    Updated project.clj and README.md with better descriptions

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # tarot
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to implement French Tarot
 
 ## Usage
 
-FIXME
+???
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 James Poole
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tarot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library designed to implement French Tarot"
+  :url "https://bitbucket.org/gipperp/tarot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
